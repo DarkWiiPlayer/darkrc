de845a580ae1e5450c4ae4f228376f49d3beb64b
DarkWiiPlayer <darkwiiplayer@hotmail.com>
2020-12-08 13:54:55 +0100

    Update thunderbird devilspie settings

--- devilspie2/thunderbird.lua
+++ devilspie2/thunderbird.lua
@@ -1,28 +1,21 @@
 local function opacity(value)
-  os.execute(string.format(
-    "xprop -id %s -f _NET_WM_WINDOW_OPACITY 32c -set _NET_WM_WINDOW_OPACITY 0x%xffffff",
-    get_window_xid(), math.max(math.min(value, 255), 0)
-  ))
+	os.execute(string.format(
+		"xprop -id %s -f _NET_WM_WINDOW_OPACITY 32c -set _NET_WM_WINDOW_OPACITY 0x%xffffff",
+		get_window_xid(), math.max(math.min(value, 255), 0)
+	))
 end
 
 local function rep(n, value)
-  if n>0 then return value, rep(n-1, value) end
+	if n>0 then return value, rep(n-1, value) end
 end
 
 local margin = { rep(4, 16) }
 if get_application_name():find("Thunderbird$") then
-  print(get_window_name())
-  if get_window_type():find("NORMAL$") then
-    set_window_workspace(get_workspace_count())
-    unmaximize()
-    if get_window_name():find("Thunderbird$") then
-      undecorate_window()
-      set_window_geometry2(1680+margin[1], margin[2], 900-margin[1]-margin[3], 1440-margin[2]-margin[4])
-    else
-      set_window_geometry(1680+margin[1], margin[2], 900-margin[1]-margin[3], 1440-margin[2]-margin[4])
-      set_window_fullscreen(true)
-    end
-    --maximize()
-    opacity(230)
-  end
+	if get_window_type():find("NORMAL$") then
+		set_window_workspace(get_workspace_count())
+		unmaximize()
+		set_window_fullscreen(true)
+		maximize()
+		opacity(230)
+	end
 end
