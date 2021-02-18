6c3cd8769d10b2ecb5bd52d5e93ad29d22350007
DarkWiiPlayer <darkwiiplayer@hotmail.com>
2021-02-18 17:59:38 +0100

    Remove status from conky player info

--- conky/music.lua
+++ conky/music.lua
@@ -1,5 +1,5 @@
 return [=====[
 ${execi 1 playerctl metadata xesam:artist | sed 's/-/—/'}
 ${font Quicksand:size=20}${execi 1 playerctl metadata xesam:title | sed 's/-/—/'}${font}
-${alignr}${execi 1 playerctl status}
+#${alignr}${execi 1 playerctl status}
 ]=====]
