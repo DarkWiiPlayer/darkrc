25e31beaff47bf5068d410dbaeb470678698ac1f
DarkWiiPlayer <darkwiiplayer@hotmail.com>
2023-05-13 09:33:21 +0200

    Add Neotree plugin to vim

--- vim/lua/plugins.lua
+++ vim/lua/plugins.lua
@@ -10,6 +10,15 @@ use {
 	'neovim/nvim-lspconfig';
 	{ 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } };
 	{ 'nvim-treesitter/nvim-treesitter', { cmd = 'TSUpdate' } };
+	{ 'nvim-neo-tree/neo-tree.nvim',
+		branch = "v2.x",
+		requires = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+		}
+	},
+	'leafo/moonscript-vim';
 	'pigpigyyy/Yuescript-vim';
 	'vim-scripts/openscad.vim';
 	'wbthomason/packer.nvim';
