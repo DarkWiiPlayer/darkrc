75917bd441145cbfaa60513745e7e2bef24aff22
DarkWiiPlayer <darkwiiplayer@hotmail.com>
2024-02-07 19:41:26 +0100

    Switch neotree and outline around

--- vim/lua/pack/setup/neotree.lua
+++ vim/lua/pack/setup/neotree.lua
@@ -1,6 +1,7 @@
 return function()
 	require('neo-tree').setup {
 		window = {
+			position = "right";
 			width = 25;
 		};
 		filesystem = {
--- vim/lua/pack/setup/outline.lua
+++ vim/lua/pack/setup/outline.lua
@@ -2,6 +2,7 @@ return function()
 	vim.keymap.set("n", "<leader>o", "<cmd>OutlineOpen<CR><cmd>OutlineFocus<CR>", { desc = "Toggle Outline" })
 	require("outline").setup {
 		outline_window = {
+			position = "left";
 			width = 20;
 			relative_width = false;
 		};
