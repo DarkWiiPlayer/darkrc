6c9dc5a0f39e8c1c924f9ede24e9b1cd20483659
DarkWiiPlayer <darkwiiplayer@hotmail.com>
2024-07-22 22:51:59 +0200

    Add wezterm configuration

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,27 @@
+local wezterm = require "wezterm"
+
+local config = wezterm.config_builder()
+
+config.hide_tab_bar_if_only_one_tab = true
+
+local padding = 2
+config.window_padding = {
+	left = (padding * 1) .. "cell",
+	right = (padding * 1) .. "cell",
+	top = (padding * .4) .. "cell",
+	bottom = (padding * .4) .. "cell"
+}
+
+config.font = wezterm.font "Fira Code"
+
+local darkfile = os.getenv("HOME") .. "/.dark"
+
+wezterm.add_to_config_reload_watch_list(darkfile)
+
+if io.open(darkfile) then
+	config.color_scheme = "Apprentice (base16)"
+else
+	config.color_scheme = "Atelier Plateau Light (base16)"
+end
+
+return config
