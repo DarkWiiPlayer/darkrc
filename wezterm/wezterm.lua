db7e8ba9571d76fc983f7fad4a1cceff4487509b
DarkWiiPlayer <darkwiiplayer@hotmail.com>
2025-03-18 11:56:15 +0100

    Update wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,14 +6,6 @@ local config = wezterm.config_builder()
 
 config.hide_tab_bar_if_only_one_tab = true
 
-local padding = 2
-config.window_padding = {
-	left = (padding * 1) .. "cell",
-	right = (padding * 1) .. "cell",
-	top = (padding * .4) .. "cell",
-	bottom = (padding * .4) .. "cell"
-}
-
 config.font = wezterm.font "Fira Code"
 
 config.default_prog = { 'zsh', '--login' }
@@ -28,5 +20,51 @@ if io.open(darkfile) then
 else
 	config.color_scheme = "Londontube (light) (terminal.sexy)"
 end
+
+local function recompute_window(window)
+	local override = window:get_config_overrides() or {}
+
+	local dimensions = window:get_dimensions()
+
+	local padding
+	if dimensions.is_full_screen then
+		padding = math.min(dimensions.pixel_width, dimensions.pixel_height) * 0.05
+	else
+		padding = 10
+	end
+	override.window_padding = {
+		left = padding,
+		right = padding,
+		top = padding,
+		bottom = padding
+	}
+
+	window:set_config_overrides(override)
+end
+
+wezterm.on("window-resized", recompute_window)
+wezterm.on("window-config-reload", recompute_window)
+
+config.window_decorations = "TITLE | RESIZE"
+
+config.keys = {
+	{
+		key = "d",
+		mods = "ALT",
+		action = wezterm.action.EmitEvent "toggle-window-decorations"
+	};
+}
+
+wezterm.on("toggle-window-decorations", function(window)
+	local override = window:get_config_overrides() or {}
+
+	if override.window_decorations == "RESIZE" then
+		override.window_decorations = "TITLE | RESIZE"
+	else
+		override.window_decorations = "RESIZE"
+	end
+
+	window:set_config_overrides(override)
+end)
 
 return config
