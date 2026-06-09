3cfa2df27521371aa8b18901afebb700a2039dbe
DarkWiiPlayer <darkwiiplayer@hotmail.com>
2026-06-09 11:45:36 +0200

    Update vim matcha

--- vim/plugin/matcha.lua
+++ vim/plugin/matcha.lua
@@ -1,5 +1,13 @@
 local function nextcol(state)
-	local from, to = state.str:find(state.pattern, state.last)
+	local from, to
+	if type(state.pattern) == "string" then
+		from, to = state.str:find(state.pattern, state.last)
+	else
+		if state.last == 0 then
+			from, to = state.pattern:match_str(state.str)
+			if from then from = from + 1 end
+		end
+	end
 	if from then
 		state.last = to+1
 		return from, to
@@ -14,12 +22,12 @@ local function colmatch(str, pattern)
 	return nextcol, {str=str,pattern=pattern,last=0}
 end
 
-local function findLocations(pattern, first_glob, ...)
-	local paths if first_glob then
-		paths = vim.fn.glob(first_glob, false, true)
-		if ... then
-			for _, glob in ipairs { ... } do
-				for _, path in ipairs(vim.fn.glob(glob, false, true)) do
+local function findFiles(...)
+	local paths if ... then
+		paths = {}
+		for _, glob in ipairs { ... } do
+			for _, path in ipairs(vim.fn.glob(glob, false, true)) do
+				if isfile(path) then
 					-- Assume overhead of table.insert is outweighed by all the file ops anyway
 					table.insert(paths, path)
 				end
@@ -29,22 +37,28 @@ local function findLocations(pattern, first_glob, ...)
 		paths = vim.fn.argv()
 	end
 
+	return paths
+end
+
+local function findLocations(pattern, files, regex)
 	local locations = {}
 
-	for _, path in ipairs(paths) do
+	if regex then
+		pattern = vim.regex(pattern)
+	end
+
+	for _, file in ipairs(files) do
 		local lnum = 0
-		if isfile(path) then
-			for line in io.lines(path) do
-				lnum = lnum + 1
-				for from, to in colmatch(line, pattern) do
-					table.insert(locations, {
-						filename = path;
-						lnum = lnum;
-						col = from;
-						end_col = to;
-						text = line:sub(from, to)
-					})
-				end
+		for line in io.lines(file) do
+			lnum = lnum + 1
+			for from, to in colmatch(line, pattern) do
+				table.insert(locations, {
+					filename = file;
+					lnum = lnum;
+					col = from;
+					end_col = to;
+					text = line:sub(from, to)
+				})
 			end
 		end
 	end
@@ -60,6 +74,7 @@ local parse_args do
 			{ "Use location list instead of quickfix list", "--location-list", "-l" };
 			{ "Appends to the current quicklist", "--append", "-a" };
 			{ "Replaces the current quicklist", "--replace", "-r" };
+			{ "Use vim regular expressions instead of patterns", "--vim-regex", "-v" };
 		}
 	else
 		function parse_args(args)
@@ -71,7 +86,9 @@ end
 
 vim.api.nvim_create_user_command("Matcha", function(params)
 	local args = parse_args(params.fargs)
-	local locations = findLocations(unpack(args))
+	local files = findFiles(unpack(args, 2))
+
+	local locations = findLocations(args[1], files, args.vimregex)
 
 	if not locations[1] then return print("Nothing found") end
 
